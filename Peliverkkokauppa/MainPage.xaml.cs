cfc166e4d9a7d8311995262b9194f5235794073f
Teemu Remes <K2131@student.jamk.fi>
2016-03-29 19:34:21 +0300

    jou

--- Peliverkkokauppa/MainPage.xaml.cs
+++ Peliverkkokauppa/MainPage.xaml.cs
@@ -26,8 +26,7 @@ namespace Peliverkkokauppa
         {
             this.InitializeComponent();
 
-            //asdsadsa
-            //as
+
         }
     }
 }
---
+++ Peliverkkokauppa/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peliverkkokauppa
+{
+    class Person
+    {               
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int Phonenumber { get; set; }
+        public string Address { get; set; }
+
+
+        public Person()
+        {
+        }
+
+        
+        public Person(string firstName, string lastName, string username, string password, string email, int phonenumber, string address)
+        {
+            Firstname = firstName;
+            Lastname = lastName;
+            Username = username;
+            Password = password;
+            Email = email;
+            Phonenumber = phonenumber;
+            Address = address;
+        }
+        
+        public void ChangeInformation()
+        {
+
+        }
+
+        public void RestorePassword()
+        {
+
+        }
+
+        /*
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + Address + " " + Age + " " + PhoneNumber;
+        }
+        */
+    }
+
+}
