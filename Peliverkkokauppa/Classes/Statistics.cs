3224c4f857b72618e9972834a50f3c626cd84bf9
gitvalt <valtteri.seuranen@gmail.com>
2016-04-12 19:35:01 +0300

    Tiedon salaamisen testaamista

--- Peliverkkokauppa/Classes/Statistics.cs
+++ Peliverkkokauppa/Classes/Statistics.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Security.Authentication;
+using Windows.Security.Cryptography.Core;
 namespace Peliverkkokauppa
 {
     public class Statistics
     {
         public Dictionary<int,Game> ListOfGames { get; set; }
         public Dictionary<string, Developer> ListOfDevelopers { get; set; }
+        internal Dictionary<int, Customer> ListOfCustomers { get; set; }
+
+        public bool Authentication(string Username, string Password)
+        {
+
+           /* 
+            byte[] buffer = System.Text.Encoding.ASCII.GetBytes(Password);
+            CryptographicKey
+            CryptographicHash hash = CryptographicEngine.Encrypt(Password, 0, buffer.Length);
+
+            string encrypted;
+            */
+                
+            return true;
+        }
 
     }
 }
