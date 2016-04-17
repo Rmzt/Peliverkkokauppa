eca6172ab5b07a04d66d9e3295576606116dc13b
K1967 <valtteri.seuranen@gmail.com>
2016-04-18 00:44:43 +0300

    Datahashing test

--- Peliverkkokauppa/login1.xaml.cs
+++ Peliverkkokauppa/login1.xaml.cs
@@ -15,6 +15,9 @@ using Windows.UI.Xaml.Navigation;
 using System.IO.Compression;
 using Windows.Data.Text;
 using System.Text;
+using Windows.Security.Cryptography.Core;
+
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Peliverkkokauppa
@@ -27,13 +30,17 @@ namespace Peliverkkokauppa
         public Statistics statistics = new Statistics();
         public SQL_queryies sql = new SQL_queryies();
         public bool isConnected { get; set; }
-        
+                
 
 
         public login1()
         {
             this.InitializeComponent();
             TestSQLCon();
+            
+
+           
+
         }
 
         private void NeAcc_Click(object sender, RoutedEventArgs e)
@@ -46,7 +53,18 @@ namespace Peliverkkokauppa
         private void Login_Click(object sender, RoutedEventArgs e)
         {
             //Lisätään koodi, jolla tarkistetaan käyttäjätunnuksen olemassaolo
+            /*
 
+            //https://msdn.microsoft.com/en-us/library/windows/apps/windows.security.cryptography.core.hashalgorithmnames.md5
+            var md5 = HashAlgorithmNames.Md5;
+            string Pass = PasswordBox.Password;
+
+            var hasher = HashAlgorithmProvider.OpenAlgorithm(Pass);
+            
+            var hashed = hasher.CreateHash();
+            UsernameBox.Text = hashed.ToString();
+*/
+            
 
             TestSQLCon();
 
@@ -54,6 +72,7 @@ namespace Peliverkkokauppa
             string Password = PasswordBox.Password;
 
             Authenticate Auth = new Authenticate();
+            
 
             /*
 
@@ -85,7 +104,7 @@ namespace Peliverkkokauppa
                 PasswordBox.Password = "";
             }
 
-
+            
             
         }
 
